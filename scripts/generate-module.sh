108362ed8a67fc2ed04facbd205b35825bf26e39
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-01-22 16:00:23 -0600

    WIP generate script changes

--- scripts/generate-module.sh
+++ scripts/generate-module.sh
@@ -11,8 +11,18 @@ else
 
     if [[ -f "generate/pingone-$3.yml" ]]; then \
         echo "==> Running codegen-$3..."
-        openapi-generator-cli version-manager set 7.0.1
-        openapi-generator-cli generate -i generate/pingone-$3.yml -g go --additional-properties=packageName=$3,packageVersion=$version,isGoSubmodule=true,enumClassPrefix=true,apiNameSuffix=Api -o . --git-repo-id $2 --git-user-id $1 --http-user-agent \"pingtools PingOne-GOLANG-SDK-$3/$version\"; \
+        #openapi-generator-cli version-manager set 7.0.1
+        #openapi-generator-cli generate -i generate/pingone-$3.yml -g go --additional-properties=packageName=$3,packageVersion=$version,isGoSubmodule=true,enumClassPrefix=true,apiNameSuffix=Api -o . --git-repo-id $2 --git-user-id $1 --http-user-agent \"pingtools PingOne-GOLANG-SDK-$3/$version\"; \
+
+        docker run --rm \
+            -v "$PWD:/local" openapitools/openapi-generator-cli:v7.0.1 generate \
+            -i /local/generate/pingone-$3.yml \
+            -g go \
+            -o /local/ \
+            --git-user-id $1 \
+            --git-repo-id $2 \
+            --http-user-agent "pingtools PingOne-GOLANG-SDK-$3/$version" \
+            --additional-properties=packageName=$3,packageVersion=$version,isGoSubmodule=true,enumClassPrefix=true,apiNameSuffix=Api
 
         echo "==> Copying custom templated files..."
         template=$(cat ../scripts/templates/client_ext.go.tmpl)
